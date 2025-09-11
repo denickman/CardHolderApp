b02fdb79f2c37aa4c21211f332b8b08f2c1ff6f3
Dennis Yaremenko <yaremdennis@gmail.com>
2025-09-11 11:26:46 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:cardholder/pages/home_page.dart';
+import 'package:go_router/go_router.dart';
+import 'package:cardholder/pages/scan_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      // debugShowCheckedModeBanner - на экране сверху справа появляется полупрозрачная красная ленточка с надписью DEBUG.
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      routerConfig: _router,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+    );
+  }
+
+  final _router = GoRouter(
+    debugLogDiagnostics: true,
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        name: HomePage.routeName,
+        path: HomePage.routeName,
+        builder: (ctx, state) => const HomePage(),
+        routes: [
+          GoRoute(
+            name: ScanPage.routeName,
+            path: ScanPage.routeName,
+            builder: (ctx, state) => const ScanPage(),
+          ),
+        ],
+      ),
+    ],
+  );
+}
